f60b23696f4056dff78750d6537ff7d598edd6e6
NaboriRuta <naboriruta0@icloud.com>
2023-08-26 16:38:43 -0600

    complete dropdown code

--- dropdown-script.js
+++ dropdown-script.js
@@ -1,3 +1,7 @@
-function activateDropdown() {
-    document.getElementById("dropdown").classList.toggle("show");
+function activateDropdown1() {
+    document.getElementById("dropdown1").classList.toggle("show");
+}
+
+function activateDropdown2() {
+    document.getElementById("dropdown2").classList.toggle("show");
 }
