bb9d744c3f7f17c62d60fc30df6d2f6276155ad4
NaboriRuta <naboriruta0@icloud.com>
2023-09-10 21:56:08 -0600

    about us, login w/o sql, and renaming

---
+++ login.js
@@ -0,0 +1,50 @@
+function setFormMessage(formElement, type, msg) {
+    const msgElement = formElement.querySelector('.form-msg');
+    msgElement.textContent = msg;
+    msgElement.classList.remove('success-msg', 'incorrect-credential');
+    msgElement.classList.add(type)
+}
+
+function setInputError(inputElement, msg) {
+    inputElement.classList.add('input-err');
+    inputElement.parentElement.querySelector(".err-msg").textContent = msg;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove('err-msg');
+    inputElement.parentElement.querySelector(".err-msg").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector('#login');
+    const newAccForm = document.querySelector('#newAcc');
+
+    document.querySelector('#createAccount').addEventListener('click', e => {
+        e.preventDefault();
+        loginForm.classList.add('hidden');
+        newAccForm.classList.remove('hidden');
+    });
+    document.querySelector('#toLogin').addEventListener('click', e => {
+        e.preventDefault();
+        newAccForm.classList.add('hidden');
+        loginForm.classList.remove('hidden');
+    });
+
+    loginForm.addEventListener('submit', e => {
+        e.preventDefault();
+        setFormMessage(loginForm, 'incorrect-credential', "Incorrect Username/Password");
+    });
+    document.querySelectorAll('.input').forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === 'createPass' && e.target.value.length > 0 && e.target.value.length < 8) {
+                setInputError(inputElement, "Password must be at least 8 characters long");
+            }
+            if (e.target.id === "confirm" && !(e.target.value === document.querySelector('#createPass').value)) {
+                setInputError(inputElement, "Password must match")
+            }
+        });
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
