44ab348d3d0c546b0e669c85a8d5228ed0480d9c
NaboriRuta <naboriruta0@icloud.com>
2023-11-28 21:36:23 -0700

    added comments

--- dropdown-script.js
+++ dropdown-script.js
@@ -1,3 +1,7 @@
+/* 
+All the dropdown scripts are defined in this one file
+Copy and paste the function as needed for as many dropdowns as there are
+*/
 function activateDropdown1() {
     document.getElementById("dropdown1").classList.toggle("show");
 }
